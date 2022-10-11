19f5b2a0a753502750a0f5d76a2a1bfba30bb4e3
Jeimmy Alejandra Ramirez Bernal <y.d.aleja@gmail.com>
2022-10-10 22:31:57 -0500

    Se creo componente login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './login/login.component.html',
+  styleUrls: ['./login/login.component.scss']
 })
 export class AppComponent {
   title = 'Glosas-front';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import {MatInputModule} from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
