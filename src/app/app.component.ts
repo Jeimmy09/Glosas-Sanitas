cbaadc0ca03b584a1711226486e459420aa1488f
ticdesarrollo03 <y.d.aleja@gmail.com>
2022-10-12 14:05:57 -0500

    Creación de dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, route } from '@angular/core';
+import {  OnInit } from '@angular/core';
+import {  Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './login/login.component.html',
   styleUrls: ['./login/login.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Glosas-front';
+  constructor(private route: Router)
+}
+
+
+  redi(){
+    this.route.navigate(['../Dashboard']);
+
+
 }
